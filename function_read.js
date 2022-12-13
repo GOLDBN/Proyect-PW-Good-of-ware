55183b51bd4a560a9949f918ebbc7176bb2be0ec
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-12-13 00:05:19 -0600

    Actualziacion MASIVA

--- function_read.js
+++ function_read.js
@@ -2,15 +2,89 @@ fetch("./datos.json")
 .then(function(response){
    return response.json();
 })
+
 .then(function(products){
    let placeholder = document.querySelector("#data-output");
    let out = "";
    for(let product of products){
       out += `
-         <tr>
-            <td> <img style='width:100px;' src='${product.image}'> </td>
-            <td >${product.name}</td>
-         </tr>
+
+      <div class="carousel-item">
+         <div class="row">
+           <div class="col-md-6">
+             <h1 class="controller_text">${product.name}</h1>
+             <p class="banner_text">${product.descripcion}</div>
+           </div>
+
+           <div class="col-md-6">
+             <div class="image_1"><img src='${product.image}'></div>
+           </div>
+         </div>
+      </div>
+      `;
+   }
+ 
+   placeholder.innerHTML = out;
+});
+
+
+fetch("./Nominaciones.json")
+.then(function(response){
+   return response.json();
+})
+
+.then(function(nominaciones){
+   let placeholder = document.querySelector("#nominaciones");
+   let out = "";
+   for(let nominacion of nominaciones){
+      out += `
+      <tr>
+         <td >${nominacion.nominacion}</td>
+      </tr>
+      `;
+   }
+ 
+   placeholder.innerHTML = out;
+});
+
+fetch("./caracteristicas.json")
+.then(function(response){
+   return response.json();
+})
+
+.then(function(caracteristicas){
+   let placeholder = document.querySelector("#caracteristicas");
+   let out = "";
+   for(let carac of caracteristicas){
+      out += `
+
+         <div class="col-md-6">
+         <div class="image_2"><img style="width: 160px;" src='${carac.image}'></div>
+         <h1 class="game_text">${carac.caract}</h1>
+         <p class="long_text">${carac.descripcion}</p>
+       </div>
+      `;
+   }
+ 
+   placeholder.innerHTML = out;
+});
+
+fetch("./alumnos.json")
+.then(function(response){
+   return response.json();
+})
+
+.then(function(alumnnos){
+   let placeholder = document.querySelector("#alumnnos");
+   let out = "";
+   for(let alu of alumnnos){
+      out += `
+
+      <div class="col-md-6">
+      <div class="image_2"><img style="width: 160px;" src='${alu.image}'></div>
+      <h1 class="game_text">${alu.alumno}</h1>
+ 
+    </div>
       `;
    }
  
